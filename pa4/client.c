48e19c0e9876f995d884804424e25cb913e32028
Carrotsmile <djd.dauria@gmail.com>
2017-04-30 14:42:13 -0400

    Created client driver for testing

--- pa4/client.c
+++ pa4/client.c
@@ -1,5 +1,30 @@
+#include "libnetfiles.h"
 
 int main(int argc, char * argv[])
 {
-  return 0;
+	//test only for use on local machine
+	char * hostname = "localhost";
+	netserverinit(hostname, 1);
+
+	char * file1 = "files/book1.pdf";
+	int fd1 = netopen(file1, O_RDWR);
+
+	char * file2 = "files/book2.djvu";
+	int fd2 = netopen(file2, O_RDWR);
+
+	char * file3 = "files/book3.pdf";
+	FILE * file = fopen(file3, "r");
+	fseek(file, 0L, SEEK_END);
+	int size = ftell(file);
+
+	char * txt = calloc(1, size);
+	
+	printf("Read: %zd\n", netread(fd1, txt, size));
+	printf("Write: %zd\n", netwrite(fd2, txt, size));
+	printf("[%d] %s\n", errno, strerror(errno));
+
+	int fdc1 = netclose(fd1);
+	printf("Netclosed %d\n [%d]%s\n", fdc1, errno, strerror(errno));
+	free(txt);	
+	return 0;
 }
